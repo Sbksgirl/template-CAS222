ce9a3ee7cb4189da196999195cdf80f10bb8abdf
Sbksgirl <61181307+Sbksgirl@users.noreply.github.com>
2020-02-23 22:49:13 -0800

    Add files via upload

---
+++ includes/footer.inc.html.php
@@ -0,0 +1,2 @@
+<footer><p>Copyright &copy; <?php echo date("Y"); ?>
+Ace in the Hole Myltisport Event</p></footer>
---
+++ includes/nav.inc.html.php
@@ -0,0 +1,27 @@
+<nav class="site-nav">
+			<div class="logo"><img src="pictures/logo.png"></div>
+			
+			<div id="social-icons">
+                <p>
+                    <a id ="icon" href="<?php echo BASE_URL; ?>https://www.facebook.com/Cas222Aceinthehole-110661963841617/"><i class="fab fa-facebook-square"></i></a>
+                    <a id ="icon" href="<?php echo BASE_URL; ?>cas222 @pcccas222"><i class="fab fa-twitter-square"></i></a>
+				</p>
+			</div>
+
+			<div class="menu-toggle">
+				<div class="hamburger"></div>
+			</div>
+
+			<ul class="open desktop">
+				<li><a href="<?php echo BASE_URL; ?>#">Home</a></li>
+				<li><a href="<?php echo BASE_URL; ?>#">About the Event</a></li>
+				<li><a href="<?php echo BASE_URL; ?>#">Packets</a></li>
+			<ul clas="nav2">
+				<li><a  href="<?php echo BASE_URL; ?>#">Registration</a></li>
+				<li><a href="<?php echo BASE_URL; ?>#">What to Bring</a></li>
+				<li><a  href="<?php echo BASE_URL; ?>#">FAQs</a></li>
+				<li><a  href="<?php echo BASE_URL; ?>#">Contact</a></li>
+			</ul>
+			</ul>
+				
+		</nav>
